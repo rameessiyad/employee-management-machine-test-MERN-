d5f1c322bf6ee4f4a39170dc7cecb333df605d20
rameessiyad <rameesmsiyadofficial786@gmail.com>
2024-09-25 15:14:33 +0530

    edit employee functionaliy added

--- server/controllers/employee-controller.js
+++ server/controllers/employee-controller.js
@@ -29,5 +29,27 @@ module.exports = {
     listEmployees: asyncHandler(async (req, res) => {
         const employees = await Employee.find({}).sort({ createdAt: -1 });
         res.status(200).json(employees);
+    }),
+
+    editEmployee: asyncHandler(async (req, res) => {
+        const { id } = req.params;
+
+        const employee = await Employee.findById(id);
+        if (!employee) return res.status(404).json({ message: "Employee not found" });
+
+        //image upload
+        const imageUrl = req.file ? getFileUrl(req, req.file) : null;
+
+        const updatedEmployee = await Employee.findByIdAndUpdate(id, {
+            f_image: imageUrl,
+            f_name: req.body.f_name || employee.f_name,
+            f_email: req.body.f_email || employee.f_email,
+            f_mobile: req.body.f_mobile || employee.f_mobile,
+            f_designation: req.body.f_designation || employee.f_designation,
+            f_gender: req.body.f_gender || employee.f_gender,
+            f_course: req.body.f_course || employee.f_course
+        }, { new: true });
+
+        res.status(200).json({ message: "Employee updated", employee: updatedEmployee });
     })
 }
--- server/routes/employee-route.js
+++ server/routes/employee-route.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const isAuth = require('../middlewares/auth-middleware');
 const { upload } = require('../utils/image-upload');
-const { createEmployee, listEmployees } = require('../controllers/employee-controller');
+const { createEmployee, listEmployees, editEmployee } = require('../controllers/employee-controller');
 
 const router = express.Router();
 
-router.post('/create', isAuth, upload.single('f_image'), createEmployee)
-router.get('/list', isAuth, listEmployees)
+router.post('/create', isAuth, upload.single('f_image'), createEmployee);
+router.get('/list', isAuth, listEmployees);
+router.put('/edit/:id', isAuth, editEmployee);
 
 module.exports = router
